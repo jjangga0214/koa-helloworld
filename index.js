66ab60b98133607e12980335e869e35766004a1a
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-07-21 16:28:04 +0900

    feat(routes, middlewares): extend
    routers
      - /reqBody
      - /something
    
    middlewwares
      - koa-morgan: logger
      - koa-body: body parser
      - koa-router: router

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const Koa = require('koa')
+const morgan = require('koa-morgan')
+const koaBody = require('koa-body')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
-})
+router
+  .get('/', async (ctx, next) => {
+    // ctx.router available
+    ctx.body = 'Hello World'
+  })
+  .all('/reqBody', async (ctx, next) => {
+    // ctx.router available
+    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`
+  })
+  .get('/something', async (ctx, next) => {
+    ctx.body = {
+      something: 'something here',
+    }
+  })
+
+app
+  .use(morgan('combined'))
+  .use(koaBody())
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
